e2305c6f1603d1047df7c250896b2b816fa19326
seph <sephvelut@gmail.com>
2018-08-28 10:41:25 -0400

    use object for vad

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,24 @@ pub mod fvad {
     #![allow(non_snake_case)]
     include!(concat!("../output", "/bindings.rs"));
 
-    pub unsafe fn is_silent(fvad: *mut Fvad, buffers: &[i16]) -> ::std::os::raw::c_int {
-        let buffer = &buffers[0] as *const i16;
-
-        fvad_process(fvad, buffer, buffers.len())
+    pub struct Vad {
+        fvad: *mut Fvad
     }
 
-    pub unsafe fn new() -> *mut Fvad {
-        fvad_new()
-    }
+    impl Vad {
+        pub unsafe fn new(sample_rate: i32) -> Vad {
+            let fvad: *mut Fvad = fvad_new();
+            fvad_set_sample_rate(fvad, sample_rate);
 
-    pub unsafe fn set_sample_rate(fvad: *mut Fvad, sample_rate: i32) {
-        fvad_set_sample_rate(fvad, sample_rate);
+            Vad {
+                fvad
+            }
+        }
+
+        pub unsafe fn is_silent(self, buffers: &[i16]) -> ::std::os::raw::c_int {
+            let buffer = &buffers[0] as *const i16;
+
+            fvad_process(self.fvad, buffer, buffers.len())
+        }
     }
 }
